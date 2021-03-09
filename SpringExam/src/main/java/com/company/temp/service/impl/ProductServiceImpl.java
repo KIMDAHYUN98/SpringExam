b13c202ac2ea4e556c5022231ca6df4b8eeba6c2
kimdahyun <hgzxc76@naver.com>
2021-03-09 18:07:06 +0900

    2021-03-09

---
+++ SpringExam/src/main/java/com/company/temp/service/impl/ProductServiceImpl.java
@@ -0,0 +1,27 @@
+package com.company.temp.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.company.temp.service.ProductService;
+import com.company.temp.vo.ProductVO;
+
+@Service
+public class ProductServiceImpl implements ProductService {
+
+	@Autowired ProductMapper dao;
+	
+	@Override
+	public void insertPro(ProductVO vo) {
+		dao.insertPro(vo);
+		
+	}
+
+	@Override
+	public List<ProductVO> getSearchPro(ProductVO vo) {
+		return dao.getSearchPro(vo);
+	}
+
+}
